eb8d4c788bacbf3b44b665202067f73d6642d07d
neojinya <wonder.yes.world@gmail.com>
2019-11-01 11:41:57 +0900

    変更しました（リンクのみ）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    mypage_path(resource)
+  end
+
+  def after_sign_in_path_for(resource)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kata, :last_name_kata, :nikename, :phone_number])
+  end
 end
