2b00d5482e8b8128590c5e3707ba778f594f93ae
horitakuma <horitaku5013@gmail.com>
2019-11-04 14:26:18 +0900

    create_user_signup_function

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products, only: [:index, :new]
+  resources :users, only: [:index, :show, :create, :edit, :update]
 
   # 仮置き
   get '/mypage', to: "products#mypage"
   get '/mypage/profile', to: "products#profile"
- 
+  get '/sign_up/registration', to: "users#registration"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
