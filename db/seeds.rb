a37d6f61e8135edfa99a1bf79c210aaac2969684
santakurow <newtakuproj0730@gmail.com>
2019-11-05 05:59:26 +0900

    商品一覧を表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env == "development"
+  # 10.times do |i|
+  #   Product.create!(
+  #     name: "ほげほげ#{i + 1}",
+  #     price: 8600,
+  #     size: 25,
+  #     condition: "きれい"
+  #   )
+  # end
+  # 10.times do |i|
+  #   Image.create!(image: "https://lh3.googleusercontent.com/rhevmCJG49m8PjNkt_Ago2mg2JgH_KrVbVETL5tNsMJfNVYzzWR9JK6YKRLiZz-5P54", product_id: "#{i}")
+  # end
+end
