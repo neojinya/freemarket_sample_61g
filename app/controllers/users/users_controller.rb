e6b9d0e3c5e6400a300bc52865bd7b0fa30b37cf
neojinya <wonder.yes.world@gmail.com>
2019-11-07 14:01:50 +0900

    修正しました！

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,11 +1,2 @@
 class UsersController < ApplicationController
-  # def login
-  #   @user = User.find_by(email: [:email], password: [:password])
-  #     if @user
-  #       flash[:notice] = "ログインしました"
-  #       redirect_to("/products/index")
-  #     else
-  #       render("sessions/new")
-  #     end
-  # end
 end
