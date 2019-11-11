1d995c4bde5454a4a48f04a073d7dce9debd1b6d
initiald-tl52 <d.sch.d.52@gmail.com>
2019-11-11 12:26:59 +0900

    add null false to seller_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191109094100) do
+ActiveRecord::Schema.define(version: 20191111032132) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "parent_id"
     t.string   "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
@@ -41,7 +42,7 @@ ActiveRecord::Schema.define(version: 20191109094100) do
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                           null: false
     t.integer  "price",                          null: false
-    t.integer  "size"
+    t.integer  "size",                           null: false
     t.integer  "condition",                      null: false
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
@@ -50,8 +51,8 @@ ActiveRecord::Schema.define(version: 20191109094100) do
     t.integer  "delivery_fee_pay"
     t.integer  "region"
     t.integer  "category_id"
+    t.integer  "seller_id_id",                   null: false
     t.integer  "buyer_id_id"
-    t.integer  "seller_id_id"
     t.index ["buyer_id_id"], name: "index_products_on_buyer_id_id", using: :btree
     t.index ["seller_id_id"], name: "index_products_on_seller_id_id", using: :btree
   end
