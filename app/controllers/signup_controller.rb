16982bf3df55f0d4e14963a028f9b4f2fa8a2133
initiald-tl52 <d.sch.d.52@gmail.com>
2019-11-16 17:40:48 +0900

    add validates_step1

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,5 +1,7 @@
 # deviseとは別のsign_up用のコントローラー
 class SignupController < ApplicationController
+  before_action :validates_step1, only: :step2
+  before_action :validates_step2, only: :step3
 
   def step1
     @user = User.new
@@ -9,11 +11,6 @@ class SignupController < ApplicationController
   # TODO: Date.new〜以降はuserモデルに記述する必要あり 
   # TODO: :password_confirmationの保存方法 現状はDBに保存していない
   def step2
-    session[:nickname] = user_params[:nickname]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password]
-    session[:password_confirmation] = user_params[:password_confirmation]
-    session[:birthday] = Date.new(params[:user]["birthday(1i)"].to_i,params[:user]["birthday(2i)"].to_i,params[:user]["birthday(3i)"].to_i)
     @user = User.new
   end
 
@@ -64,8 +61,32 @@ class SignupController < ApplicationController
     sign_in User.find(session[:id]) unless user_signed_in?
   end
 
-private
+  def validates_step1
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:birthday] = Date.new(params[:user]["birthday(1i)"].to_i,params[:user]["birthday(2i)"].to_i,params[:user]["birthday(3i)"].to_i)
+    @user = User.new(
+      nickname:          session[:nickname],
+      email:             session[:email],
+      password:          session[:password],
+      birthday:          session[:birthday],
 
+      last_name:         "山田",
+      first_name:        "太郎",
+      last_name_kata:    "ヤマダ",
+      first_name_kata:   "タロウ",
+      phone_number:      "01234567891",
+      post_code:         "0123456",
+      prefecture:        "東京都",
+      city_village_town: "渋谷区",
+      house_number:      "青山1-1-1",
+      )
+
+    render '/signup/step1' unless @user.valid?
+  end
+
+private
   def user_params
     params.require(:user).permit(
       :nickname,
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,20 @@ class User < ApplicationRecord
   has_many :credits
   has_many :products
   has_many :cards
-  # validates :email, presence: true
-  # validates :encrypted_password, presence: true
-  #                                length: { minimum: 6}
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true, length: { minimum: 7}
   # validates :reset_password_token, presence: true
   # validates :reset_password_sent_at, presence: true
   # validates :remember_created_at, presence: true
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :first_name_kata, presence: true
-  # validates :last_name_kata, presence: true
-  # validates :nickname, presence: true
-  # validates :post_code, presence: true
-  # validates :prefecture, presence: true
-  # validates :city_village_town, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kata, presence: true
+  validates :last_name_kata, presence: true
+  validates :nickname, presence: true
+  validates :post_code, presence: true
+  validates :prefecture, presence: true
+  validates :city_village_town, presence: true
   # validates :house_number, presence: true
 
 end
