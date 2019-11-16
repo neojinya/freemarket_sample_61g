588ebf14ab0095f2e2a9c74118ac4d0f26a530cc
initiald-tl52 <d.sch.d.52@gmail.com>
2019-11-16 11:41:09 +0900

    set params to session at step2

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -3,10 +3,18 @@ class SignupController < ApplicationController
 
   def step1
     @user = User.new
+
   end
 
+  # TODO: Date.new〜以降はuserモデルに記述する必要あり 伊藤 20191116
   def step2
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:birthday] = Date.new(params[:user]["birthday(1i)"].to_i,params[:user]["birthday(2i)"].to_i,params[:user]["birthday(3i)"].to_i)
     @user = User.new
+    binding.pry
   end
 
   def step3
@@ -19,15 +27,20 @@ class SignupController < ApplicationController
 
 private
 
+def flatten_date_array hash
+  %w(1 2 3).map { |e| hash["date(#{e}i)"].to_i }
+end
+
+
   def user_params
     params.require(:user).permit(
       :nickname,
       :email,                
       :password,   
+      :last_name,
       :first_name,           
-      :last_name,            
-      :first_name_kata,      
-      :last_name_kata,                    
+      :last_name_kata,            
+      :first_name_kata,          
       :phone_number,         
       :post_code,            
       :prefecture,           
