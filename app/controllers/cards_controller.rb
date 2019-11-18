c1c07ccadc38d0ef2916dcb77ce67c7e8c99e0cb
initiald-tl52 <d.sch.d.52@gmail.com>
2019-11-18 20:26:39 +0900

    display message on create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -14,10 +14,12 @@ class CardsController < ApplicationController
     else
       customer = Payjp::Customer.create(card: params['payjp-token'])
       card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if card.save!
+      if card.save
         redirect_to action: "show"
+        flash[:notice] = 'カードを登録しました。'
       else
         redirect_to action: "create"
+        flash[:notice] = 'カードの登録に失敗しました。'
       end
     end
   end
