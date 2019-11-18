09c926b9c14685d1812544f3d9cada3cf469bd88
initiald-tl52 <d.sch.d.52@gmail.com>
2019-11-18 10:30:01 +0900

    resolve migration error

--- db/migrate/20191115061828_add_seller_id_to_products.rb
+++ db/migrate/20191115061828_add_seller_id_to_products.rb
@@ -1,7 +1,7 @@
 class AddSellerIdToProducts < ActiveRecord::Migration[5.0]
   def change
-    add_column :products, :seller_id, :integer, :null=>false
-    add_column :products, :bland_id, :integer
-    add_column :products, :buyer_id, :integer
+    change_column :products, :seller_id, :integer, :null=>false
+    change_column :products, :bland_id, :integer
+    change_column :products, :buyer_id, :integer
   end
 end
