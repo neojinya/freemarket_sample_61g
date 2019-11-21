be5092e36f4e040a04f085e09c0418505d792f1a
Tsushima164 <tsushimahiroshi60@gmail.com>
2019-11-21 16:28:47 +0900

    commit

--- db/migrate/20191115032040_remone_size_from_products.rb
+++ db/migrate/20191115032040_remone_size_from_products.rb
@@ -1,7 +1,7 @@
 class RemoneSizeFromProducts < ActiveRecord::Migration[5.0]
-  def up
-    remove_column :products, :size
-      end
+  # def up
+  #   remove_column :products, :size
+  #     end
 
   def down
     add_column :products, :size, :integer
