056fda9241dcaab9c1867034bc3a25487196a95b
Tsushima164 <tsushimahiroshi60@gmail.com>
2019-11-21 16:12:05 +0900

    commit

--- db/migrate/20191111042854_add_seller_column_to_products.rb
+++ db/migrate/20191111042854_add_seller_column_to_products.rb
@@ -1,5 +1,5 @@
 class AddSellerColumnToProducts < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_reference :products, :seller, foreign_key: { to_table: :users },null: false
   end
 end
